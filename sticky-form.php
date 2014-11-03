30697d38fe309be447ccefef7e4faa4cfedae496
fried_eggz <fried_eggz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-03 00:35:38 +0000

    New version: 1.0.4 Improvment: Use the Gravity Forms API to update form (Please RocketGenius, fix the bug in your code) New option: Choose if the entry should be marked as unread upon save
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-sticky-form/trunk@1018515 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sticky-form.php
+++ sticky-form.php
@@ -4,16 +4,17 @@
   Plugin URI: https://github.com/13pixlar/gravity-forms-sticky-form
   Description: This is a <a href="http://www.gravityforms.com/" target="_blank">Gravity Form</a> plugin that enables forms to be "sticky". A sticky form stays populated with the users submitted data retrieved from the actual entry.
   Author: Adam Rehal
-  Version: 1.0.3
+  Version: 1.0.4
   Author URI: http://13pixlar.se
   Orginal Plugin by: asthait & unclhos
  */
 
 
-
+// Definne global variable $valid and set to true. We need to do this to prevent the form values from disapearing in there is a validation error
 global $valid;
 $valid = 1;
 
+// Check if the form is valid, and if it is set the variable $valid to true, if not, set to false
 add_filter( 'gform_validation', 'test_valid' );
 
 function test_valid($form) {
@@ -27,6 +28,7 @@ function test_valid($form) {
 }
 
 
+// Lets atempt to pre populate the form
 add_filter("gform_pre_render", "sticky_pre_populate_the_form");
 
 function sticky_pre_populate_the_form($form) {
@@ -35,46 +37,59 @@ function sticky_pre_populate_the_form($form) {
         if ($current_page == 1) {
 
             global $valid;
-          
+
+            // Get the stored entry ID 
             $entry_id = sticky_getEntryOptionKeyForGF($form);
-          
+
+            // If the form has been submited, is valid and we are not in the preview area
             if($valid && strpos($_SERVER['REQUEST_URI'],'preview') == false) {
-              
+
+                // We have a previously saved entry
                 if (get_option($entry_id)) {
 
+                    // Get the entry 
                     $form_fields = RGFormsModel::get_lead(get_option($entry_id));
-                  
+
+                    // If an entry is found we need prepare if for insertion into the form
                     if($form_fields  && $form_fields["status"] != "trash") {
-                      
+
+                        
+
+                        // Create new correctly formated keys and get rid of the old ones
                         foreach ($form_fields as $key => &$value) {
                             
+                            // If the key is numeric we need to change it from [X.X] to [input_X_X]
                             if (is_numeric($key)) {
 
                                 $new_key = str_replace(".", "_", "input_$key");
 
                                 $form_fields[$new_key] = $form_fields[$key];
                                 unset($form_fields[$key]);                            
-                              
+                                
+                                // If we have an upload field
                                 if(strpos($value, "uploads/")) {
                                     $upload = $value;
                                 }                               
                             }
                         }
                         
+                        // Add is_submit_id field
                         $form_id = $form['id'];
                         $form_fields["is_submit_$form_id"] = "1";
 
                         $_POST = $form_fields;
-                  
-                    } else {
+
+                    // If no entry is found; unset the stored entry ID
+                    }else {
                         update_option($entry_id, "");
                     }
                 }
             }
         }
     }
-  
-    if($upload) {
+
+    // Replace {upload} with reference to uploaded file
+    if (isset($upload)) {
         foreach ($form["fields"] as &$field) {
             foreach ($field as $key => &$value) {
                 if($key == "content") {
@@ -83,59 +98,78 @@ function sticky_pre_populate_the_form($form) {
             }
         }     
     }
+
     return $form;
 }
 
 
-add_filter( 'gform_entry_id_pre_save_lead', 'save_with_same_id', 10, 2 );
-function save_with_same_id( $entry_id, $form ) {
-    
-    $saved_option_value = get_option(sticky_getEntryOptionKeyForGF($form));
-
-  
-    if ($form['isSticky'] && !$form['isEnableMulipleEntry'] && $saved_option_value) {
-        
-        $update_entry_id = $saved_option_value;
-        return $update_entry_id ? $update_entry_id : $entry_id;
-    }
-}
-
-
+// If we dont have a saved entry for this form, we need to save the ID of the one that have been created
+// If we do have a saved value however, we need to  update the old entry and delete the new entry
 add_action("gform_post_submission", "sticky_set_post_content", 10, 2);
-
 function sticky_set_post_content($entry, $form) {
 
     if ($form['isSticky']) {
         
         if (is_user_logged_in()) {
 
+
             $saved_option_key = sticky_getEntryOptionKeyForGF($form);
             $saved_option_value = get_option($saved_option_key);
-          
+
+            // Save the entry ID in wp_options if this is the first time the form is submitted by this user.
+            // If we dont have a saved value, we save the entry ID
             if(!$saved_option_value) {
                 update_option($saved_option_key, $entry['id']);
+            
+            //If we have a saved value, update and delete
+            }else{
+                
+                // ...as long as the form doesnt allow multiple entries...
+                if(!$form['isEnableMulipleEntry']) {
+
+                    // We dont want to loose our starred and read status when we update
+                    $original_entry = RGFormsModel::get_lead($saved_option_value);
+                    $entry["is_starred"] = $original_entry["is_starred"];
+                    
+                    // ...unless the user wants us to
+                    if(!$form['isMarkUnread']) {
+                        $entry["is_read"] = $original_entry["is_read"];
+                    }
+
+                    $success = GFAPI::update_entry($entry, $saved_option_value);
+                    $success = GFAPI::delete_entry($entry["id"]);
+
+                // ...and if it does, dont delete or update but save the new entry ID instead so that the sticky form is populated with the latest saved entry.
+                }else{
+                   update_option($saved_option_key, $entry['id']); 
+                }
             }
         }
     }
 }
 
 
+// To save a reference to th entry we need the option key to be unique to both user and form
 function sticky_getEntryOptionKeyForGF($form) {
 
     global $current_user;
     get_currentuserinfo();
 
+    // Lets make the option key unique
     $saved_option_key = $current_user->user_login . '_GF_sticky_' . $form['id'] . '_entry';
     
     return $saved_option_key;
 }
 
-
+// Add Sticky checkbox to the form settings
 add_filter("gform_form_settings", "sticky_settings", 50, 2);
 
 function sticky_settings($form_settings, $form) {
 
     $tr_sticky = '
+            <tr>
+                <td colspan="2"><h4 class="gf_settings_subgroup_title">Sticky Form</h4></td>
+            </tr>
             <tr>
                 <th>Sticky Form</th>
             <td>
@@ -146,23 +180,33 @@ function sticky_settings($form_settings, $form) {
             </td>
         </tr>
         <tr>
-            <th></th>
+            <tr>
+                <th>Multi entry</th>
             <td>
             <input type="checkbox" id="form_enable_multiple_entry" onclick="SetFormMultipleEntry();" /> 
             <label for="form_enable_multiple_entry">              
-                Enable multi entry from same user while form is sticky
+                Enable multiple entries from same user while form is sticky
+            </label>
+            </td>
+        </tr>
+        <tr>
+            <tr>
+                <th>Mark unread</th>
+            <td>
+            <input type="checkbox" id="form_mark_unread" onclick="SetFormMarkUnread();" /> 
+            <label for="form_mark_unread">              
+                Mark entry as unread when updated
             </label>
             </td>
         </tr>';
         
-    $form_settings["Form Options"]['sticky'] = $tr_sticky;
-    
-    return $form_settings;
+        $form_settings["Form Options"]['sticky'] = $tr_sticky;
+        
+        return $form_settings;
 }
 
-
+// Action to inject supporting script to the form editor page
 add_action("gform_advanced_settings", "sticky_editor_script");
-
 function sticky_editor_script() {
     ?>
     <script type='text/javascript'>
@@ -173,9 +217,13 @@ function sticky_editor_script() {
         function SetFormMultipleEntry(){
             form.isEnableMulipleEntry = jQuery("#form_enable_multiple_entry").is(":checked");
         }
+        function SetFormMarkUnread(){
+            form.isMarkUnread = jQuery("#form_mark_unread").is(":checked");
+        }
                 
         jQuery("#form_sticky_value").attr("checked", form.isSticky);       
         jQuery("#form_enable_multiple_entry").attr("checked", form.isEnableMulipleEntry);
+        jQuery("#form_mark_unread").attr("checked", form.isMarkUnread);
         
     </script>
     <?php
