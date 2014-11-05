de6d7d157d0c1d8f12929a61e173f8448103e62f
fried_eggz <fried_eggz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-05 22:49:14 +0000

    Fixed undefined index notice
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-sticky-form/tags/1.0.2@1020568 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sticky-form.php
+++ sticky-form.php
@@ -10,11 +10,11 @@
  */
 
 
-// Definne global variable $valid and set to true 
+// 
 global $valid;
 $valid = 1;
 
-// Check if the form is valid, and if it is set the variable $valid to true, if not, set to false
+
 add_filter( 'gform_validation', 'test_valid' );
 
 function test_valid($form) {
@@ -34,36 +34,36 @@ function sticky_pre_populate_the_form($form) {
         $current_page = GFFormDisplay::get_current_page($form["id"]);
         if ($current_page == 1) {
 
-            // Get the entry ID 
+            
             $entry_id = sticky_getEntryOptionKeyForGF($form);
 
             global $valid;
 
-            // If the form has been submited, is valid and we are not in the preview area
+            
             if($valid && strpos($_SERVER['REQUEST_URI'],'preview') == false) {
 
                 if (get_option($entry_id)) {
 
-                    // Get the entry 
+                    
                     $form_fields = RGFormsModel::get_lead(get_option($entry_id));
 
-                    // If an antry is found we need to modify it
+                    
                     if($form_fields) {
 
-                        // Helper function to remove non needed items
+                        
                         function array_change_key(&$array, $old_key, $new_key) {
                             $array[$new_key] = $array[$old_key];
                             unset($array[$old_key]);
                             return;
                         }
 
-                        // Remove non needed items and format the keys correctly
+                        
                         foreach ($form_fields as $key => $value) {
                             
                             if (is_numeric($key)) {
                                 array_change_key($form_fields, $key, str_replace(".", "_", "input_$key"));
                                 
-                                // If the field is an upload
+                                
                                 if(strpos($value, "uploads/")) {
                                     $upload = $value;
                                 }
@@ -71,7 +71,7 @@ function sticky_pre_populate_the_form($form) {
                             }
                         }
                         
-                        // Add is_submit_id field
+                        
                         $form_id = $form['id'];
                         $form_fields["is_submit_$form_id"] = "1";
                         $_POST = $form_fields;
@@ -81,7 +81,7 @@ function sticky_pre_populate_the_form($form) {
         }
     }
 
-    // Replace {upload} with reference to uploaded file
+    
     if($upload) {
         foreach ($form["fields"] as &$field) {
             foreach ($field as $key => &$value) {
@@ -126,13 +126,13 @@ function sticky_getEntryOptionKeyForGF($form) {
     global $current_user;
     get_currentuserinfo();
 
-    // We need to make the option key unique
+    
     $option_key = $current_user->user_login . '_GF_sticky_' . $form['id'] . '_entry';
     
     return $option_key;
 }
 
-// Add Sticky checkbox to the form settings
+
 add_filter("gform_form_settings", "sticky_settings", 50, 2);
 
 function sticky_settings($form_settings, $form) {
@@ -162,7 +162,7 @@ function sticky_settings($form_settings, $form) {
         return $form_settings;
 }
 
-// Action to inject supporting script to the form editor page
+
 add_action("gform_advanced_settings", "sticky_editor_script");
 function sticky_editor_script() {
     ?>
